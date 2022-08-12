6c752d89d2b24827aec60183607cd2de132ea052
Theboral <110025150+Theboral-Ayyanar@users.noreply.github.com>
2022-08-12 17:12:17 +0530

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,26 @@
+package addition;
+
+import java.util.*;
+
+public class Third {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		  System.out.print("Enter your first value");
+		   int value1=sc.nextInt();
+		  System.out.print("Enter your second value");
+		   int value2=sc.nextInt();
+		  System.out.println("value calculation is:");
+		  System.out.println("Adition:"+(value1+value2));
+		  System.out.println("subtraction:"+(value1-value2));
+		  System.out.println("Adition:"+(value1+value2));
+		  System.out.println("mutiplication:"+(value1*value2));
+		   
+
+		  
+	     	
+		
+
+	}
+
+}
